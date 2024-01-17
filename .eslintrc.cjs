955c845fb3c5626369866b0ed9ded30cc755c4fd
Adishwar Rishi <adiswa123@gmail.com>
2024-01-17 18:57:08 +1100

    Move eslint to cjs file format and upgrade prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  ignorePatterns: [".eslintrc.json"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+};
