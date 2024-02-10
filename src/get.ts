4be40a62497f669d395716f4edf1ed62c33128db
Adishwar Rishi <adiswa123@gmail.com>
2024-02-10 11:38:53 +1100

    chore(deps): update dependency @epic-web/cachified to v5  (#87)
    * @epic-web/cachified  ^4.0.0  →  ^5.1.1
    
    * Add changeset for 2.0 version bump
    
    * (feat): Add generics to the cloudflare cache implementation
    
    This is in an effort to match the behavior of @epic-web/cachified since 5.1.0
    
    * Add changeset for generis
    
    * (feat): Add provenance to npm publish config

--- src/get.ts
+++ src/get.ts
@@ -2,17 +2,17 @@ import { KVNamespace } from "@cloudflare/workers-types";
 import { type CacheEntry, type CacheMetadata } from "@epic-web/cachified";
 import { buildCacheKey } from "./utils";
 
-export async function getOperation(
+export async function getOperation<Value = unknown>(
   kv: KVNamespace,
   key: string,
   keyPrefix?: string,
-): Promise<CacheEntry<unknown> | null> {
+): Promise<CacheEntry<Value> | null> {
   const cacheKey = buildCacheKey(key, keyPrefix);
   const { value, metadata } = await kv.getWithMetadata<CacheMetadata>(cacheKey, { type: "text" });
   if (value === null) {
     return value;
   }
-  const jsonValue = JSON.parse(value) as unknown;
+  const jsonValue = JSON.parse(value) as Value;
   return {
     value: jsonValue,
     metadata: metadata!,
